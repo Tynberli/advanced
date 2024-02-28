2cb0d0b723a8e526af12a319ff2da58c0f80ddbb
Tynberli <tynberli@mail.ru>
2024-02-28 21:19:01 +0300

    2-dice

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 'use strict'
-const users = [
-    { id: 1, name: "Vasya" },
-    { id: 2, name: "Petya" },
-    { id: 1, name: "Vasya" }
-]
-console.log(users);
-const usersToString = users.map(JSON.stringify);
-console.log(usersToString);
-const usersSet = new Set(usersToString);
-console.log(usersSet);
-const usersArr = Array.from(usersSet);
-console.log(usersArr);
-const usersUniq = usersArr.map(JSON.parse);
-console.log(usersUniq);
+function rollDice(diceType) {
+    if (diceType !== 'd4' && diceType !== 'd6' && diceType !== 'd8' && diceType !== 'd10' && diceType !== 'd12' && diceType !== 'd16' && diceType !== 'd20') {
+        return 'Неподдерживаемый тип кубика. Пожалуйста, выберите d4, d6, d8, d10, d12, d16 или d20.';
+    }
+
+    switch (diceType) {
+        case 'd4':
+            return Math.floor(Math.random() * 4) + 1;
+        case 'd6':
+            return Math.floor(Math.random() * 6) + 1;
+        case 'd8':
+            return Math.floor(Math.random() * 8) + 1;
+        case 'd10':
+            return Math.floor(Math.random() * 10) + 1;
+        case 'd12':
+            return Math.floor(Math.random() * 12) + 1;
+        case 'd16':
+            return Math.floor(Math.random() * 16) + 1;
+        case 'd20':
+            return Math.floor(Math.random() * 20) + 1;
+        default:
+            return 'Что-то пошло не так.';
+    }
+}
+
+const result = rollDice('d6');
+console.log(`Результат броска d6 кубика: ${result}`);
