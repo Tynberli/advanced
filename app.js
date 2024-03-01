f89f80328e6e41a897e7c47e9285c46b7c05d8a1
Tynberli <tynberli@mail.ru>
2024-03-01 03:47:30 +0300

    3-birthday

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 'use strict'
-function rollDice(diceType) {
-    if (diceType !== 'd4' && diceType !== 'd6' && diceType !== 'd8' && diceType !== 'd10' && diceType !== 'd12' && diceType !== 'd16' && diceType !== 'd20') {
-        return 'Неподдерживаемый тип кубика. Пожалуйста, выберите d4, d6, d8, d10, d12, d16 или d20.';
-    }
+const user = "2010-03-01";
+//console.log(Number(new Date(user)));
 
-    switch (diceType) {
-        case 'd4':
-            return Math.floor(Math.random() * 4) + 1;
-        case 'd6':
-            return Math.floor(Math.random() * 6) + 1;
-        case 'd8':
-            return Math.floor(Math.random() * 8) + 1;
-        case 'd10':
-            return Math.floor(Math.random() * 10) + 1;
-        case 'd12':
-            return Math.floor(Math.random() * 12) + 1;
-        case 'd16':
-            return Math.floor(Math.random() * 16) + 1;
-        case 'd20':
-            return Math.floor(Math.random() * 20) + 1;
-        default:
-            return 'Что-то пошло не так.';
+function validateYears(user) {
+    const curYer = Number(new Date());
+    //console.log(curYer);
+    const difference = curYer - user;
+    //console.log(difference);
+    const age = difference / (1000 * 60 * 60 * 24 * 365);
+    //console.log(age);
+    const fullAge = Math.round(age)
+    //console.log(fullAge);
+    if (fullAge >= 14) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-const result = rollDice('d6');
-console.log(`Результат броска d6 кубика: ${result}`);
+console.log(validateYears(Number(new Date(user))));
