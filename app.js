7f6825995d7580b739937a781ddae315f22f2767
Tynberli <tynberli@mail.ru>
2024-03-04 21:21:01 +0300

    6-class

--- app.js
+++ app.js
@@ -1,47 +1,5 @@
 'use strict'
-//Базовый класс 
-const Base = function (race, name, language) {
-    this.race = race;
-    this.name = name;
-    this.language = language;
-}
-
-Base.prototype.greet = function () {
-    console.log(`Меня зовут ${this.name}, я представитель расы ${this.race}. Говорю на ${this.language}.`);
-}
-
-//Класс Орка
-const Orc = function (race, name, language, weapon) {
-    Base.call(this, race, name, language);
-    this.weapon = weapon;
-}
-
-Orc.prototype = Object.create(Base.prototype);
-Orc.prototype.constructor = Orc;
-
-Orc.prototype.attack = function () {
-    console.log(`${this.name} атакует с помощью ${this.weapon}!`);
-}
-//Класс Эльфа
-const Elf = function (race, name, language, spellType) {
-    Base.call(this, race, name, language);
-    this.spellType = spellType;
-}
-
-Elf.prototype = Object.create(Base.prototype);
-Elf.prototype.constructor = Elf;
-
-Elf.prototype.castSpell = function () {
-    console.log(`${this.name} создает заклинание типа ${this.spellType}!`);
-}
-
-const person1 = new Base('Человек', 'Арагорн', 'Русский');
-person1.greet();
-
-const orc1 = new Orc('Орк', 'Гром', 'Орочий', 'топор');
-orc1.greet();
-orc1.attack();
-
-const elf1 = new Elf('Эльф', 'Леголас', 'Эльфийский', 'воздух');
-elf1.greet();
-elf1.castSpell();
+/* Создайте класс Car у которого есть марка модель и пробег(
+    все свойства приватные задаются в конструкторе).
+    Сделайте для него возможность менять пробег через get и set 
+    добавьте метод info который выводит в консоль марку модили и пробег */
