1a7b46629a5287966de954c14a4c26cad2255223
Tynberli <tynberli@mail.ru>
2024-03-10 18:31:11 +0300

    11-geolocation

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 'use strict'
-/*Перепишите прошлый пример на цепочку Promise  */
- const pokemon = new XMLHttpRequest();
-pokemon.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
-pokemon.send();
+/* Сделайте функцию получения координат пользователя, 
+используя API но преобразовав его в Promise. */
 
-pokemon.addEventListener('load', function () {
-    const params = JSON
-        .parse(this.responseText)
-        .abilities[0]
-        .ability;
-    console.log(`Название ${params.name}`);
-
-    const effect = new XMLHttpRequest();
-    effect.open('GET', params.url);
-    effect.send();
-
-    effect.addEventListener('load', function () {
-        const descriptionEffect = JSON
-            .parse(this.responseText)
-            .effect_entries[0]
-            .effect;
-        console.log(`Описание способности
-${descriptionEffect}`)
-    })
-}); 
+navigator.geolocation.getCurrentPosition((position) => {
+    doSomthing(position.coords.latitude, position.coords.longitude);
+});
+console.log(navigator)
