8fec7c7c61bb5ac8872458a429ea69e565f42f6f
Tynberli <tynberli@mail.ru>
2024-03-14 20:33:24 +0300

    12-buttons

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 'use strict'
-/* Сделайте функцию получения координат пользователя, 
-используя API но преобразовав его в Promise. */
 
-navigator.geolocation.getCurrentPosition((position) => {
-    doSomthing(position.coords.latitude, position.coords.longitude);
-});
-console.log(navigator)
+const wrapper = document.querySelector('.wrapper');
+console.log(wrapper);
+for (let i = 0; i < 5; i++) {
+    const el = document.createElement('button');
+    el.innerText = `Нажми меня`;
+    el.classList.add('button');
+    wrapper.append(el);
+}
+const btn = document.querySelectorAll('.button');
+const text = document.querySelector('.text');
+let count = 0;
+
+btn.forEach((button) => {
+    button.innerText = 'Нажми меня';
+    button.addEventListener('click', () => {
+        button.innerText = 'Нажата';
+        text.innerText = `Количество нажатий ${count += 1}`
+    })
+})
