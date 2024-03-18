5a37d7dff52b0fcb36b4dde3e20c1b18890dae9b
Tynberli <tynberli@mail.ru>
2024-03-18 20:55:41 +0300

    6-class fix

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 'use strict'
-/* Создайте класс Car у которого есть марка модель и пробег(
-    все свойства приватные задаются в конструкторе).
-    Сделайте для него возможность менять пробег через get и set 
-    добавьте метод info который выводит в консоль марку модили и пробег */
+class Car {
+    constructor(brand, model, mileage) {
+        this._brand = brand;
+        this._model = model;
+        this._mileage = mileage;
+    }
+
+    get mileage() {
+        return this._mileage;
+    }
+
+    set mileage(newMileage) {
+        if (newMileage >= 0) {
+            this._mileage = newMileage;
+        }
+    }
+
+    info() {
+        console.log(`Brand: ${this._brand}, Model: ${this._model}, Mileage: ${this._mileage}`);
+    }
+}
+
+const myCar = new Car('Toyota', 'Corolla', 50000);
+myCar.info();
+
+myCar.mileage = 60000;
+myCar.info();
