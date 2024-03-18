b0bef4d81a54113ef6e5df8880ce1d7004fc4258
Tynberli <tynberli@mail.ru>
2024-03-18 20:43:47 +0300

    4-timer fix

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-'use strict'
+'use strict';
+
 function updateCountdown() {
     const now = new Date();
     const newYear = new Date(now.getFullYear() + 1, 0, 1);
     const timeLeft = newYear - now;
-    const mouth = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 31));
-    const days = Math.floor(timeLeft % (1000 * 60 * 60 * 31) / (1000 * 60 * 60));
+    const months = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 30.436875));
+    const days = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 30.436875)) / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
     document.querySelector('.countdown').innerHTML =
         `До нового года осталось: 
-        ${mouth} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
+        ${months} месяцев, ${days} дней, ${hours} часов, ${minutes} минут, ${seconds} секунд`;
 }
 
 updateCountdown();
