fb197c06bc930e05e5de2cf092c8b78d8869e0f5
Tynberli <tynberli@mail.ru>
2024-03-25 18:48:16 +0300

    12-promise-rise fix

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict'
-/* Сделайте функцию получения координат пользователя, 
-используя API но преобразовав его в Promise. */
+function race(promises) {
+    return new Promise((resolve, reject) => {
+        promises.forEach(promise => {
+            promise.then(
+                value => resolve(value),
+                error => reject(error)
+            );
+        });
+    });
+}
 
-navigator.geolocation.getCurrentPosition((position) => {
-    doSomthing(position.coords.latitude, position.coords.longitude);
-});
-console.log(navigator)
+const promise1 = new Promise((resolve, reject) => setTimeout(resolve, 1000, 'one'));
+const promise2 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'two'));
+
+race([promise1, promise2])
+    .then(value => console.log(value))
+    .catch(error => console.error(error));
