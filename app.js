43a41136b50cf34db21e66bd0eef8538cc322233
Tynberli <tynberli@mail.ru>
2024-03-25 18:29:58 +0300

    10-pokemon-promise fix

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 'use strict'
 /*Перепишите прошлый пример на цепочку Promise  */
- const pokemon = new XMLHttpRequest();
-pokemon.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
-pokemon.send();
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject('Ошибка при загрузке данных');
+            }
+        };
+        xhr.onerror = function () {
+            reject('Ошибка сети');
+        };
+        xhr.send();
+    });
+}
 
-pokemon.addEventListener('load', function () {
-    const params = JSON
-        .parse(this.responseText)
-        .abilities[0]
-        .ability;
-    console.log(`Название ${params.name}`);
-
-    const effect = new XMLHttpRequest();
-    effect.open('GET', params.url);
-    effect.send();
-
-    effect.addEventListener('load', function () {
-        const descriptionEffect = JSON
-            .parse(this.responseText)
-            .effect_entries[0]
-            .effect;
-        console.log(`Описание способности
-${descriptionEffect}`)
+fetchData('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then(pokemonData => {
+        const abilityUrl = pokemonData.abilities[0].ability.url;
+        return fetchData(abilityUrl);
     })
-}); 
+    .then(abilityData => {
+        const description = abilityData.effect_entries[0].effect;
+        console.log(`Описание способности: ${description}`);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
