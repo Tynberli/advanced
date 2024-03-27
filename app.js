42a917c7b901dd9707c5b3b9613c731ce44f279f
Tynberli <tynberli@mail.ru>
2024-03-27 18:21:14 +0300

    1-map fix

--- app.js
+++ app.js
@@ -5,11 +5,5 @@ const users = [
     { id: 1, name: "Vasya" }
 ]
 console.log(users);
-const usersToString = users.map(JSON.stringify);
-console.log(usersToString);
-const usersSet = new Set(usersToString);
-console.log(usersSet);
-const usersArr = Array.from(usersSet);
-console.log(usersArr);
-const usersUniq = usersArr.map(JSON.parse);
-console.log(usersUniq);
+const uniqueUsers = Array.from(new Set(users.map(JSON.stringify))).map(JSON.parse);
+console.log(uniqueUsers);
